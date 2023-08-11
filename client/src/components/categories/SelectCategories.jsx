67eba4b3dade76d855d3c56f1690f549e15f25f6
cesarsanchezdiaz <sanchezdiaz104@gmail.com>
2023-08-11 05:51:23 -0700

    test vercel fe

--- client/src/components/categories/SelectCategories.jsx
+++ client/src/components/categories/SelectCategories.jsx
@@ -9,7 +9,9 @@ export default function SelectCategories() {
   useEffect(() => {
     const getCategories = async () => {
       try {
-        const res = await axios.get('/categories');
+        const res = await axios.get(
+          'https://cesar-tracy-blog.vercel.app/api/categories'
+        );
         const namesToArray = Array.from(res.data.map((cat) => cat.name));
         setCategories(namesToArray);
       } catch (error) {
