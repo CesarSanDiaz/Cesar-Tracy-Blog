31299d04e6985ffae7cf8e8166b9450d73704d8e
cesarsanchezdiaz <sanchezdiaz104@gmail.com>
2023-08-11 06:29:51 -0700

    s

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://cesar-tracy-blog-fe.vercel.app/api/',
+  baseURL: 'https://cesar-tracy-blog-fe.vercel.app/',
   // timeout: 1000,
   // headers: { 'X-Custom-Header': 'foobar' },
 });
