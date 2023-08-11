c5db6da27841a1aafa6bc341e6515c59c3ebb0da
cesarsanchezdiaz <sanchezdiaz104@gmail.com>
2023-08-11 05:17:37 -0700

    vercel

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -59,7 +59,7 @@ router.delete('/:id', async (req, res) => {
 //GET POST
 router.get('/:id', async (req, res) => {
   try {
-    const post = await Post.findById(req.params.id);
+    const post = await Post.findById(req.params.id).sort({ createdAt: -1 });
     res.status(200).json(post);
   } catch (err) {
     res.status(500).json(err);
