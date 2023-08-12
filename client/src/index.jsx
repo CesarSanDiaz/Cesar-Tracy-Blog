73f74a8dc11a738978d4c5fffc05147df734a4ba
cesarsanchezdiaz <sanchezdiaz104@gmail.com>
2023-08-11 19:29:24 -0700

    test

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ContextProvider } from './context/Context';
 
@@ -9,10 +8,8 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
-      <ContextProvider>
-        <App />
-      </ContextProvider>
-    </BrowserRouter>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>
 );
